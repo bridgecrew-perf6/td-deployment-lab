e09a8a2c9f0f9cadac657a0107fde92818db3ce6
Tori Daughton <toriddaughton@gmail.com>
2022-04-27 16:00:52 -0600

    image added

--- server/index.js
+++ server/index.js
@@ -4,8 +4,18 @@ const path = require(`path`)
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../client/index.html'))
 })
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/styles.css'))
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/main.js'))
+})
+
+
 
 const port = process.env.PORT || 4025
 
